90e23ffba3f843206c4bf026500602f2df9bae9d
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-01-04 10:07:47 -0500

    mejora en legibilidad

--- 02-bases/src/js-foundation/06-promises.js
+++ 02-bases/src/js-foundation/06-promises.js
@@ -1,10 +1,10 @@
 const getPokemonId = (id, callback) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-  fetch(url).then((response) => {
-    response.json().then((pokemon) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((pokemon) => {
       callback(pokemon.name);
     });
-  });
 };
 
 module.exports = getPokemonId;
