83450515e493ef38614ae208cd954f81e5499059
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-01-04 12:34:46 -0500

    anexop de la libreria de winston

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,6 +1,12 @@
-const getPokemonId = require("./js-foundation/06-promises");
+// const getPokemonId = require("./js-foundation/06-promises");
 
-getPokemonId(4)
-  .then((pokemon) => console.log({ pokemon }))
-  .catch((err) => console.log({ err }))
-  .finally(() => console.log("Proceso terminado"));
+// getPokemonId(4)
+//   .then((pokemon) => console.log({ pokemon }))
+//   .catch((err) => console.log({ err }))
+//   .finally(() => console.log("Proceso terminado"));
+
+const { buildLogger } = require("./plugins");
+
+const logger = buildLogger("app,js");
+
+logger.log("Hola mundo");
--- 02-bases/src/plugins/index.js
+++ 02-bases/src/plugins/index.js
@@ -1,9 +1,11 @@
 const { getUuId } = require("../plugins/getIdPlugin");
 const { getAge } = require("../plugins/getAgePlugin");
 const { http } = require("../plugins/http-client.plugin");
+const buildLogger = require("../plugins/logger.plugin");
 
 module.exports = {
   getAge,
   getUuId,
   http,
+  buildLogger,
 };
