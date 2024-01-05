fdd2736159f4b8c2b9edc002b4385d0f617abf79
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-01-04 19:34:36 -0500

    creacion de ambiente de pruebas con jest

--- 02-bases/src/app.ts
+++ 02-bases/src/app.ts
@@ -1,17 +1,17 @@
-import { getPokemonId } from "./js-foundation/06-promises";
-import { buildLogger } from "./plugins/logger.plugin";
-// const getPokemonId = require("./js-foundation/06-promises");
+// import { getPokemonId } from "./js-foundation/06-promises";
+// import { buildLogger } from "./plugins/logger.plugin";
+// // const getPokemonId = require("./js-foundation/06-promises");
 
-// getPokemonId(4)
-//   .then((pokemon) => console.log({ pokemon }))
-//   .catch((err) => console.log({ err }))
-//   .finally(() => console.log("Proceso terminado"));
+// // getPokemonId(4)
+// //   .then((pokemon) => console.log({ pokemon }))
+// //   .catch((err) => console.log({ err }))
+// //   .finally(() => console.log("Proceso terminado"));
 
-// const { buildLogger } = require("./plugins");
+// // const { buildLogger } = require("./plugins");
 
-const logger = buildLogger("app.ts");
+// const logger = buildLogger("app.ts");
 
-logger.log("Hola mundo");
-logger.error("Esto es algo malo");
+// logger.log("Hola mundo");
+// logger.error("Esto es algo malo");
 
-console.log("hola mundo");
+// console.log("hola mundo");
