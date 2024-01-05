6a4322dfa90cbdd2324c35221ac61b7c27e3a225
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-01-04 19:47:28 -0500

    se crea la prueba para el test y se cambia la configuracion en tsconfig para los errores

--- 02-bases/test/app.test.ts
+++ 02-bases/test/app.test.ts
@@ -1,5 +1,11 @@
 describe("App", () => {
   it("should be true", () => {
-    expect(true).toBe(true);
+    // expect(true).toBe(true);
+    const num1 = 10;
+    const num2 = 20;
+
+    const result = num1 + num2;
+
+    expect(result).toBe(30);
   });
 });
