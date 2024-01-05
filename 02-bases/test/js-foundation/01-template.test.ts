407cb6ae41675db3f22d4281a77713f93a9555c5
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-01-04 19:56:53 -0500

    creacion de archivo de testing

---
+++ 02-bases/test/js-foundation/01-template.test.ts
@@ -0,0 +1,12 @@
+import { emailTemplate } from "../../src/js-foundation/01-template";
+
+describe("js-foundation/01-template.ts", () => {
+  test("emailTemplate should contain a greeting", () => {
+    expect(emailTemplate).toContain("Hi, ");
+  });
+
+  test("emailTemplate should contain {{{name}} and {{orderId}}", () => {
+    expect(emailTemplate).toMatch(/{{name}}/);
+    expect(emailTemplate).toMatch(/{{orderId}}/);
+  });
+});
