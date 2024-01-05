806b3df9f026bdbfb5bc2822b51f49237bb1e9cc
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-01-05 14:54:39 -0500

    test para http-client

--- 02-bases/src/plugins/http-client.plugin.ts
+++ 02-bases/src/plugins/http-client.plugin.ts
@@ -1,14 +1,16 @@
-//patron adaptador
-// const axios = require("axios");
 import axios from "axios";
-
 export const httpClientPlugin = {
   get: async (url: string) => {
     const { data } = await axios.get(url);
     return data;
   },
-
-  post: async (url: string, body: any) => {},
-  put: async (url: string, body: any) => {},
-  delete: async (url: string, body: any) => {},
+  post: async (url: string, body: any) => {
+    throw new Error("Not implemented");
+  },
+  put: async (url: string, body: any) => {
+    throw new Error("Not implemented");
+  },
+  delete: async (url: string, body: any) => {
+    throw new Error("Not implemented");
+  },
 };
