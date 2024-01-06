900a12316a11a2e67c7f7309a59c63ceeca13f47
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-01-05 22:01:02 -0500

    uso de node para crear una carpeta y un archivo

--- 04-multiplication/src/app.ts
+++ 04-multiplication/src/app.ts
@@ -1,38 +1,21 @@
-const fs = require("fs");
+import fs from "fs";
 
-// console.log("================================");
-// console.log("\t Tabla del 5 ");
-// console.log("================================\n");
-// for (let i = 1; i <= 10; i++) {
-//   console.log(`5 x ${i} = ${5 * i}`);
-// }
-
-// Contenido que deseas escribir en el archivo
-const contenido = `
+let outputMessage = "";
+const base = 5;
+const header = `
 ===============================
-     Tabla del 5
-===============================
-
-5 x 1 = 5
-5 x 2 = 10
-5 x 3 = 15
-5 x 4 = 20
-5 x 5 = 25
-5 x 6 = 30
-5 x 7 = 35
-5 x 8 = 40
-5 x 9 = 45
-5 x 10 = 50
+     Tabla del ${base}
+===============================\n
 `;
 
-// Ruta y nombre del archivo que deseas crear
-const rutaArchivo = "./outputs/tabla-5.txt";
+for (let i = 1; i <= 10; i++) {
+  outputMessage += `${base} x ${i} = ${5 * i}\n`;
+}
 
-// Método para escribir en el archivo
-fs.writeFile(rutaArchivo, contenido, (err: any) => {
-  if (err) {
-    console.error("Error al crear el archivo:", err);
-  } else {
-    console.log("Archivo creado exitosamente.");
-  }
-});
+outputMessage = header + outputMessage;
+
+const outputPath = "outputs";
+
+fs.mkdirSync(outputPath, { recursive: true });
+fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage);
+console.log("Archivo creado");
