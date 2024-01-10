e8cb73fe8a1cb5cec539e9bd6b97d1502919a39e
Yorlin Garcia <yorlingarcia96@hotmail.com>
2024-01-10 18:18:12 -0500

    actualzacion de las pruebs

--- 04-multiplication/src/presentation/server-app.test.ts
+++ 04-multiplication/src/presentation/server-app.test.ts
@@ -17,43 +17,43 @@ describe('server App', () => {
         jest.clearAllMocks()
     })
 
-    // test('should create serverAppp instance', () => {
-    //     const serverApp = new ServerApp();
+    test('should create serverAppp instance', () => {
+        const serverApp = new ServerApp();
 
-    //     expect(serverApp).toBeInstanceOf(ServerApp)
-    //     expect(typeof ServerApp.run).toBe('function')
-    // })
+        expect(serverApp).toBeInstanceOf(ServerApp)
+        expect(typeof ServerApp.run).toBe('function')
+    })
 
-    // test('should run serverApp default options', () => {
+    test('should run serverApp default options', () => {
         
-    //     const logSpy = jest.spyOn(console, 'log')
-    //     const tableSpy = jest.spyOn( CreateTable.prototype, 'execute')
-    //     const saveFileSpy = jest.spyOn(SaveFile.prototype ,'execute')
+        const logSpy = jest.spyOn(console, 'log')
+        const tableSpy = jest.spyOn( CreateTable.prototype, 'execute')
+        const saveFileSpy = jest.spyOn(SaveFile.prototype ,'execute')
         
-    //     const options = {
-    //         base: 2,
-    //         limit: 10,
-    //         showTable: false,
-    //         fileDestination: 'test-destination',
-    //         fileName: 'test-filename',
-    //     }
+        const options = {
+            base: 2,
+            limit: 10,
+            showTable: false,
+            fileDestination: 'test-destination',
+            fileName: 'test-filename',
+        }
 
-    //     ServerApp.run(options);
+        ServerApp.run(options);
 
-    //     expect(logSpy).toHaveBeenCalledTimes(2);
-    //     expect(logSpy).toHaveBeenCalledWith('Server running...');
-    //     expect(logSpy).toHaveBeenLastCalledWith('Archivo creado');
+        expect(logSpy).toHaveBeenCalledTimes(2);
+        expect(logSpy).toHaveBeenCalledWith('Server running...');
+        expect(logSpy).toHaveBeenLastCalledWith('Archivo creado');
 
-    //     expect(tableSpy).toHaveBeenCalledTimes(1)
-    //     expect(tableSpy).toHaveBeenCalledWith({base: options.base, limit: options.limit})
+        expect(tableSpy).toHaveBeenCalledTimes(1)
+        expect(tableSpy).toHaveBeenCalledWith({base: options.base, limit: options.limit})
 
-    //     expect(saveFileSpy).toHaveBeenCalledTimes(1);
-    //     expect(saveFileSpy).toHaveBeenCalledWith({
-    //         fileContent: expect.any(String),
-    //         fileDestination: options.fileDestination,
-    //         fileName: options.fileName
-    //     })
-    // })
+        expect(saveFileSpy).toHaveBeenCalledTimes(1);
+        expect(saveFileSpy).toHaveBeenCalledWith({
+            fileContent: expect.any(String),
+            fileDestination: options.fileDestination,
+            fileName: options.fileName
+        })
+    })
 
 
     test('should run with custom values mocked', () => {
@@ -77,7 +77,8 @@ describe('server App', () => {
             fileDestination: options.fileDestination,
             fileName: options.fileName
         })
-        expect(logErrorMock).toHaveBeenCalledWith()
+        // expect(logErrorMock).toHaveBeenCalledWith()
+
 
     })
 })
