4f5972976b46bb62c029687227d839ac0c00cbf7
Yorlin Garcia <yorlingarcia96@hotmail.com>
2024-01-10 18:28:43 -0500

    actualizacion en las pruebas de app

--- 04-multiplication/src/app.test.ts
+++ 04-multiplication/src/app.test.ts
@@ -1,10 +1,26 @@
+import { ServerApp } from "./presentation/server-app"
 
 
 
-describe('App', () => {
+describe('test App.ts', () => {
 
 
-    test('should be true', () => {
-        expect(true).toBe(true)
+    test('should call server.run with values', async () => {
+        
+        const serverRunMock = jest.fn()
+        
+        ServerApp.run = serverRunMock;
+        
+        process.argv = ['node', 'app.ts', '-b', '10', '-l', '5', '-s', '-n', 'test-file', '-d', 'test-dir']
+
+        await import('./app')
+
+        expect(serverRunMock).toHaveBeenCalledWith({
+            "base": 10, 
+            "fileDestination": "test-dir", 
+            "fileName": "test-file", 
+            "limit": 5, 
+            "showTable": true
+        })
     })
 })
