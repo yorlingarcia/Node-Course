0d82bc8c09c36c0fb6cd5b42a634ff1296e784c3
Yorlin Garcia <yorlingarcia96@hotmail.com>
2024-01-15 18:00:09 -0500

    eliminacion de comentarios

--- 05-NOC/src/presentation/server.ts
+++ 05-NOC/src/presentation/server.ts
@@ -19,35 +19,9 @@ export class Server {
 
     // ToDo: HAcer Email
     // console.log(envs.MAILER_EMAIL, envs.MAILER_SECRET_KEY);
-    new SenEmailLogs(emailService, fileSystemLogRepository).execute([
-      "yorlingarcia32@gmail.com",
-      "yorlingarcia96@hotmail.com",
-    ]);
-    // emailService.sendEmailWithFileSystemLogs([
+    // new SenEmailLogs(emailService, fileSystemLogRepository).execute([
     //   "yorlingarcia32@gmail.com",
     //   "yorlingarcia96@hotmail.com",
     // ]);
-    // emailService.sendEmail({
-    //   to: "yorlingarcia32@gmail.com",
-    //   subject: "Logs del sistema",
-    //   htmlBody: `
-    //   <h3>Logs del sistema - NOC</h3>
-    //   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque luctus posuere diam, sed finibus sapien mattis in. Sed fringilla tincidunt commodo. </p>
-    //   <p>Ver logs adjuntos</p>
-    //   `,
-    // });
-
-    // CronService.createJob("*/5 * * * * *", () => {
-    //     const date = new Date();
-    //     console.log("5 seconds", date);
-    //   const url = "http://google.com";
-    //   const url = "http://localhost:3000";
-    //   new CheckService(
-    //     fileSystemLogRepository,
-    //     () => console.log(`${url} is ok`),
-    //     (error) => console.log(error)
-    //   ).execute(url);
-    //     new CheckService().execute("http://localhost:3000");
-    // });
   }
 }
