f74d1bef7f47bdd740dddf36fa32a60e3a27c6d6
Yorlin Garcia <yorlingarcia96@hotmail.com>
2024-01-15 22:59:44 -0500

    creacion de modelo para mongo

---
+++ 05-NOC/src/data/mongo/models/log.model.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+/**
+  level: LogSeverityLevel;
+  message: string;
+  origin: string;
+  createdAt?: Date;
+ */
+
+const logSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+  },
+  origin: {
+    type: String,
+  },
+  level: {
+    type: String,
+    enum: ["low", "medium", "high"],
+    default: "low",
+  },
+  createAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+export const LogModel = mongoose.model("Log", logSchema);
