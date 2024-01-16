e3eaee4c614f8b6c324f9468a83e0b2dd9497e54
Yorlin Garcia <yorlingarcia96@hotmail.com>
2024-01-15 23:06:44 -0500

    creacion de data y consulta de data

--- 05-NOC/src/app.ts
+++ 05-NOC/src/app.ts
@@ -1,7 +1,7 @@
 import { Server } from "./presentation/server";
 import { envs } from "./config/plugins/env.plugin";
 import "dotenv/config";
-import { MongoDataBase } from "./data/mongo";
+import { LogModel, MongoDataBase } from "./data/mongo";
 (async () => {
   main();
 })();
@@ -11,6 +11,21 @@ async function main() {
     mongoUrl: envs.MOGO_URL,
     dbName: envs.MONGO_DB_NAME,
   });
+
+  // Crear una coleccion = tablas, documentos =  registros
+  // const newLog = await LogModel.create({
+  //   message: "Test message desde Mongo",
+  //   origin: "App.ts",
+  //   level: "low",
+  // });
+
+  // await newLog.save();
+
+  // console.log(newLog);
+
+  const logs = await LogModel.find();
+  console.log(logs);
+
   // Server.start();
   //   console.log(envs);
 }
