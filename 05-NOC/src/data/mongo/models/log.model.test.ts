1c71b0d8cc53efa79ecd5b7bf6b86113b5dc029d
Yorlin Garcia <yorlingarcia96@hotmail.com>
2024-01-17 16:42:35 -0500

    creacion de archivo test para log de models en mongo

---
+++ 05-NOC/src/data/mongo/models/log.model.test.ts
@@ -0,0 +1,14 @@
+import { mongo } from "mongoose";
+import { MongoDataBase } from "../init";
+import { envs } from "../../../config/plugins/envs.plugin";
+
+describe("log.model.ts", () => {
+  // beforeAll(() => {
+  //   await MongoDataBase.connect({
+  //     mongoUrl: envs.MONGO_URL,
+
+  //   })
+  // })
+
+  test("should return LogModel", () => {});
+});
