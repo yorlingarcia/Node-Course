cd46031db52048d0831eab9f88b8277038ea4fb6
Yorlin Garcia <yorlingarcia96@hotmail.com>
2024-01-17 14:42:27 -0500

    creacion de pruebas para los envs

--- 05-NOC/src/config/plugins/envs.plugin.test.ts
+++ 05-NOC/src/config/plugins/envs.plugin.test.ts
@@ -2,6 +2,28 @@ import { envs } from "./envs.plugin";
 
 describe("env.plugin.ts", () => {
   test("should return env options", () => {
-    console.log(envs);
+    expect(envs).toEqual({
+      PORT: 3000,
+      MAILER_EMAIL: "yorlin.garcia.dev@gmail.com",
+      MAILER_SECRET_KEY: "fpbqkcbrgnvskvff",
+      MAILER_SERVICE: "gmail",
+      PROD: false,
+      MOGO_URL: "mongodb://Yorlin:123456789@localhost:27017",
+      MONGO_DB_NAME: "NOC-test",
+      MONGO_USER: "Yorlin",
+      MONGO_PASS: "123456789",
+    });
+  });
+
+  test("should return error if not found env", async () => {
+    jest.resetModules();
+    process.env.PORT = "ABC";
+
+    try {
+      await import("./envs.plugin");
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(`${error}`).toContain('"PORT" should be a valid integer');
+    }
   });
 });
