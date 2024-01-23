5ba81aeb01a6ce55740ea736caeef347c0fc7372
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-01-22 22:42:52 -0500

    creacion de test para cron service

---
+++ 05-NOC/src/presentation/cron/cron-service.test.ts
@@ -0,0 +1,15 @@
+import { CronService } from "./cron-service";
+
+describe("cron-service.test.ts", () => {
+  const mockTick = jest.fn();
+
+  test("should create a job", (done) => {
+    const job = CronService.createJob("* * * * * *", mockTick);
+
+    setTimeout(() => {
+      expect(mockTick).toHaveBeenCalledTimes(2);
+      job.stop();
+      done();
+    }, 2000);
+  });
+});
