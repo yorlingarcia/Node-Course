2208a0f2244506f1389b537e552e0fec8d778cc9
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-01-23 10:05:47 -0500

    empleabilidad de json

--- 07-RestWeb/src/app.ts
+++ 07-RestWeb/src/app.ts
@@ -2,9 +2,13 @@ import http from "http";
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write(`<h1>URL: ${req.url}</h1>`);
-  res.end();
+  // res.writeHead(200, { "Content-Type": "text/html" });
+  // res.write(`<h1>URL: ${req.url}</h1>`);
+  // res.end();
+
+  const data = { name: "John Doe", age: 30, city: "New York" };
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(data));
 });
 
 server.listen(8080, () => {
