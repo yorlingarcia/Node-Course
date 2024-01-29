f069050e0b88929b0b0070cabcba82ca86cadf65
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-01-28 22:15:46 -0500

    creacion de los casos de uso par el CRUD

--- 07-RestWeb/src/domain/use-cases/todo/create-todo.ts
+++ 07-RestWeb/src/domain/use-cases/todo/create-todo.ts
@@ -1,4 +1,14 @@
+import { CreateTodoDto } from "../../dtos";
+import { TodoEntity } from "../../entities/todo.entity";
+import { TodoRepository } from "../../repositories/todo.repository";
 
+export interface CreateTodoUseCase {
+  execute(dto: CreateTodoDto): Promise<TodoEntity>;
+}
 
-
-
+export class CreteTodo implements CreateTodoUseCase {
+  constructor(private readonly repository: TodoRepository) {}
+  execute(dto: CreateTodoDto): Promise<TodoEntity> {
+    return this.repository.create(dto);
+  }
+}
