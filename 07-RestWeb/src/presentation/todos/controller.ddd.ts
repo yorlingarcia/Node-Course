f6021e4cad3c549d0680711065bb8a901c7ef434
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-01-28 22:05:05 -0500

    creacion de estructura para los casos de uso dentro de la arquitectura limpia

---
+++ 07-RestWeb/src/presentation/todos/controller.ddd.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import { prisma } from "../../data/postgres";
+import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
+import { TodoRepository } from "../../domain";
+
+export class TodosController {
+  //* DI
+  constructor(private readonly todoRepository: TodoRepository) {}
+
+  public getToDos = async (req: Request, res: Response) => {
+    const toDos = await this.todoRepository.getAll();
+    return res.json(toDos);
+  };
+
+  public getToDoById = async (req: Request, res: Response) => {
+    const id = +req.params.id;
+    try {
+      const toDo = await this.todoRepository.finById(id);
+      res.json(toDo);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  };
+
+  public createToDo = async (req: Request, res: Response) => {
+    const [error, createTodoDto] = CreateTodoDto.create(req.body);
+    if (error) return res.status(400).json({ error });
+    const toDo = await this.todoRepository.create(createTodoDto!);
+    res.json(toDo);
+  };
+
+  public updateToDo = async (req: Request, res: Response) => {
+    const id = +req.params.id;
+    const [error, updateTodoDto] = UpdateTodoDto.create({ ...req.body, id });
+    const updatedTodo = await this.todoRepository.updateById(updateTodoDto!);
+    return res.json(updatedTodo);
+  };
+
+  public deleteToDo = async (req: Request, res: Response) => {
+    const id = +req.params.id;
+    const deletedTodo = await this.todoRepository.deleteById(id)
+    res.json(deletedTodo)
+  };
+}
