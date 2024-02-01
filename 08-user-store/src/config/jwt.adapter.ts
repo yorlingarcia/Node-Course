446ef0756ae5105e54d4a34c957eec3133d4041a
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-01 14:47:13 -0500

    uso de jwt y seed para encriptar

--- 08-user-store/src/config/jwt.adapter.ts
+++ 08-user-store/src/config/jwt.adapter.ts
@@ -1,9 +1,12 @@
 import jwt from "jsonwebtoken";
+import { envs } from "./envs";
+
+const JWT_SEED = envs.JWT_SEED;
 
 export class JwtAdapter {
   static async generateToken(payload: any, duration: string = "2h") {
     return new Promise((resolve) => {
-      jwt.sign(payload, "SED", { expiresIn: duration }, (err, token) => {
+      jwt.sign(payload, JWT_SEED, { expiresIn: duration }, (err, token) => {
         if (err) return resolve(null);
         resolve(token);
       });
