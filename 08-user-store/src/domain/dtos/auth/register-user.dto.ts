287d53a11e042d63644b99bab85b9069486a6882
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-01 09:39:03 -0500

    creacion de reglas para el registro

---
+++ 08-user-store/src/domain/dtos/auth/register-user.dto.ts
@@ -0,0 +1,23 @@
+import { regularExps } from "../../../config";
+
+export class RegisterUserDto {
+  private constructor(
+    public readonly name: string,
+    public readonly email: string,
+    public readonly password: string
+  ) {}
+
+  static create(object: { [key: string]: any }): [string?, RegisterUserDto?] {
+    const { name, email, password } = object;
+
+    if (!name) return ["Missing name", undefined];
+    if (!email) return ["Missing email", undefined];
+    if (!email) return ["Missing email", undefined];
+    if (!regularExps.email.test(email))
+      return ["Email is not valid", undefined];
+    if (!password) return ["Missing password", undefined];
+    if (password.length < 6) return ["Password too short"];
+
+    return [undefined, new RegisterUserDto(name, email, password)];
+  }
+}
