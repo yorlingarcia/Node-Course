ce222ec0c00b63ae909c7e5c3eb5dd345f078c54
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-03 17:58:24 -0500

    creacion de seed para el testing de rutas y actualizacion del readme para correr en desarrollo

--- 08-user-store/README.md
+++ 08-user-store/README.md
@@ -4,11 +4,10 @@ Este proyecto previamente inicializado tiene todo lo necesario para trabajar con
 
 Cada paso de su configuración ya se ha realizado previamente en el curso, por lo que solo es necesario clonar el proyecto y comenzar a trabajar.
 
-
 ## Instalación
 
 1. Clonar .env.template a .env y configurar las variables de entorno
 2. Ejecutar `npm install` para instalar las dependencias
 3. En caso de necesitar base de datos, configurar el docker-compose.yml y ejecutar `docker-compose up -d` para levantar los servicios deseados.
-4. Ejecutar `npm run dev` para levantar el proyecto en modo desarrollo
-
+4. En Desarrollo llenar la base de datos con los datos de prueba con el comando `npm run seed`
+5. Ejecutar `npm run dev` para levantar el proyecto en modo desarrollo
--- 08-user-store/src/data/seed/seed.ts
+++ 08-user-store/src/data/seed/seed.ts
@@ -1,5 +1,11 @@
 import { envs } from "../../config";
-import { MongoDataBase } from "../mongo/mongo-database";
+import {
+  CategoryModel,
+  MongoDataBase,
+  ProductModel,
+  UserModel,
+} from "../mongo";
+import { seedData } from "./data";
 
 (async () => {
   await MongoDataBase.connect({
@@ -12,4 +18,42 @@ import { MongoDataBase } from "../mongo/mongo-database";
   await MongoDataBase.disconnect();
 })();
 
-async function main() {}
+const randomBetween0AndX = (x: number) => {
+  return Math.floor(Math.random() * x);
+};
+
+async function main() {
+  // 0. borrar datos
+  await Promise.all([
+    UserModel.deleteMany(),
+    CategoryModel.deleteMany(),
+    ProductModel.deleteMany(),
+  ]);
+
+  // 1. Crear usuarios
+  const users = await UserModel.insertMany(seedData.users);
+
+  // 2. Crear Categorias
+
+  const categories = await CategoryModel.insertMany(
+    seedData.categories.map((category) => {
+      return {
+        ...category,
+        user: users[0]._id,
+      };
+    })
+  );
+
+  // 3. Crear productos
+
+  const products = await ProductModel.insertMany(
+    seedData.products.map((product) => {
+      return {
+        ...product,
+        user: users[randomBetween0AndX(seedData.users.length - 1)]._id,
+        category:
+          categories[randomBetween0AndX(seedData.categories.length - 1)]._id,
+      };
+    })
+  );
+}
