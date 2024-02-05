b014d5483b3de60ef022146b3b0a98d4900463a1
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-05 15:22:38 -0500

    creacion de middleware para la identificacion de contenido

--- 08-user-store/src/presentation/file-upload/routes.ts
+++ 08-user-store/src/presentation/file-upload/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { FileUploadController } from "./controller";
 import { FileUploadService } from "../services/file-upload.service";
+import { FileUploadMiddleware } from "../middlewares/file-upload.middleware";
 
 export class FileUploadRoutes {
   static get routes(): Router {
@@ -10,6 +11,7 @@ export class FileUploadRoutes {
     const fileUploadService = new FileUploadService();
     const controller = new FileUploadController(fileUploadService);
 
+    router.use(FileUploadMiddleware.containFiles);
     // Definir las rutas
     router.post(
       "/single/:type",
