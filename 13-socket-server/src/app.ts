ac6fbdbdc3b5c7abc4fb0ee4589d5a4f38fce311
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-06 17:04:56 -0500

    creacion de servicio par usar ws e iniciar la conexion

--- 13-socket-server/src/app.ts
+++ 13-socket-server/src/app.ts
@@ -1,19 +1,23 @@
-import { envs } from './config/envs';
-import { AppRoutes } from './presentation/routes';
-import { Server } from './presentation/server';
+import { createServer } from "http";
+import { envs } from "./config/envs";
+import { AppRoutes } from "./presentation/routes";
+import { Server } from "./presentation/server";
+import { WssService } from "./presentation/services/wss.service";
 
-
-(async()=> {
+(async () => {
   main();
 })();
 
-
 function main() {
-
   const server = new Server({
     port: envs.PORT,
     routes: AppRoutes.routes,
   });
 
-  server.start();
+  const httpServer = createServer(server.app);
+  WssService.initWss({ server: httpServer });
+
+  httpServer.listen(envs.PORT, () => {
+    console.log(`Server running on port: ${envs.PORT}`);
+  });
 }
