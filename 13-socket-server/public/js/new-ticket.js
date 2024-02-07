b77997b4139ee037b22290d0f9fddc208677da4d
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-06 21:01:12 -0500

    creacion de logica en el html para la creacion del ticket

--- 13-socket-server/public/js/new-ticket.js
+++ 13-socket-server/public/js/new-ticket.js
@@ -1,3 +1,20 @@
+let socket = null;
 
+const currentTicket = document.getElementById("lbl-new-ticket");
+const createTicketBtn = document.querySelector("button");
 
-console.log('Nuevo Ticket HTML');
+async function getLastTickets() {
+  const lasTicket = await fetch("/api/ticket/last").then((resp) => resp.json());
+  currentTicket.innerText = lasTicket;
+}
+
+async function createTicket() {
+  const newTicket = await fetch("/api/ticket", {
+    method: "POST",
+  }).then((resp) => resp.json());
+  currentTicket.innerText = newTicket.number;
+}
+
+createTicketBtn.addEventListener("click", createTicket);
+
+getLastTickets();
