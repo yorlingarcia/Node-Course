33f47db9d963736a58717467ec886ff7c39fee4c
Yorlin Garcia <yorlin.garcia.dev@gmail.com>
2024-02-06 23:32:08 -0500

    implementacion de la consulta a la api y la visualizacion en index

--- 13-socket-server/public/js/public.js
+++ 13-socket-server/public/js/public.js
@@ -1,3 +1,23 @@
+async function loadCurrentTickets() {
+  const tickets = await fetch("api/ticket/working-on").then((resp) =>
+    resp.json()
+  );
+  renderTickets(tickets);
+}
 
+function renderTickets(tickets = []) {
+  for (let i = 0; i < tickets.length; i++) {
+    if (i >= 4) break;
+    const ticket = tickets[i];
 
-console.log('Público HTML')
+    if (!ticket) continue;
+
+    const lblTicket = document.querySelector(`#lbl-ticket-0${i + 1}`);
+    const lblDesk = document.querySelector(`#lbl-desk-0${i + 1}`);
+
+    lblTicket.innerText = `Ticket ${ticket.number}`;
+    lblDesk.innerText = ticket.handleAtDesk;
+  }
+}
+
+loadCurrentTickets();
